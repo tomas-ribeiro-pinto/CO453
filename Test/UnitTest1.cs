4aa26e6ccb89199c01ffed0033b5f00df6391c74
tomas-ribeiro-pinto <morato.toms@outlook.com>
2022-03-19 11:48:17 +0000

    screenshots and finishments

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -22,5 +22,135 @@ namespace Test
 
             Assert.AreEqual(actualGrade, Grades.F);
         }
+
+        [TestMethod]
+        public void TestConvertLowestPass()
+        {
+            // Arrange
+
+            StudentGrades App03 = new StudentGrades();
+
+            // Act
+
+            Grades actualGrade = App03.ConvertToGrades(40);
+
+            // Assert
+
+            Assert.AreEqual(actualGrade, Grades.D);
+        }
+
+        [TestMethod]
+        public void TestConvertHighestPass()
+        {
+            // Arrange
+
+            StudentGrades App03 = new StudentGrades();
+
+            // Act
+
+            Grades actualGrade = App03.ConvertToGrades(49);
+
+            // Assert
+
+            Assert.AreEqual(actualGrade, Grades.D);
+        }
+
+        [TestMethod]
+        public void TestConvertLowestLowerSecond()
+        {
+            // Arrange
+
+            StudentGrades App03 = new StudentGrades();
+
+            // Act
+
+            Grades actualGrade = App03.ConvertToGrades(50);
+
+            // Assert
+
+            Assert.AreEqual(actualGrade, Grades.C);
+        }
+
+
+        [TestMethod]
+        public void TestConvertHighestLowerSecond()
+        {
+            // Arrange
+
+            StudentGrades App03 = new StudentGrades();
+
+            // Act
+
+            Grades actualGrade = App03.ConvertToGrades(59);
+
+            // Assert
+
+            Assert.AreEqual(actualGrade, Grades.C);
+        }
+
+
+        [TestMethod]
+        public void TestConvertHighestUpperSecond()
+        {
+            // Arrange
+
+            StudentGrades App03 = new StudentGrades();
+
+            // Act
+
+            Grades actualGrade = App03.ConvertToGrades(60);
+
+            // Assert
+
+            Assert.AreEqual(actualGrade, Grades.B);
+        }
+
+        [TestMethod]
+        public void TestConvertLowestUpperSecond()
+        {
+            // Arrange
+
+            StudentGrades App03 = new StudentGrades();
+
+            // Act
+
+            Grades actualGrade = App03.ConvertToGrades(69);
+
+            // Assert
+
+            Assert.AreEqual(actualGrade, Grades.B);
+        }
+
+        [TestMethod]
+        public void TestConvertFirst()
+        {
+            // Arrange
+
+            StudentGrades App03 = new StudentGrades();
+
+            // Act
+
+            Grades actualGrade = App03.ConvertToGrades(70);
+
+            // Assert
+
+            Assert.AreEqual(actualGrade, Grades.A);
+        }
+
+        [TestMethod]
+        public void TestConvertErrorGrade()
+        {
+            // Arrange
+
+            StudentGrades App03 = new StudentGrades();
+
+            // Act
+
+            Grades actualGrade = App03.ConvertToGrades(200);
+
+            // Assert
+
+            Assert.AreEqual(actualGrade, Grades.X);
+        }
     }
 }
