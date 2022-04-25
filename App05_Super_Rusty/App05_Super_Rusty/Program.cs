a80107e64ea7bb65774f665782cf998a0d7c906f
tomas-ribeiro-pinto <morato.toms@outlook.com>
2022-04-25 11:24:47 +0100

    App05 start

---
+++ App05_Super_Rusty/App05_Super_Rusty/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace App05_Super_Rusty
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
