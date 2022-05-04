a71a7d73e4e20b880bcc1cf5a872ca087fa2d5f6
tomas-ribeiro-pinto <morato.toms@outlook.com>
2022-05-04 14:37:26 +0100

    App05 start

--- App05_Super_Rusty/App05_Super_Rusty/Game1.cs
+++ App05_Super_Rusty/App05_Super_Rusty/Game1.cs
@@ -9,6 +9,9 @@ namespace App05_Super_Rusty
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Texture2D _texture;
+        private Vector2 _position;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -27,24 +30,44 @@ namespace App05_Super_Rusty
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            _texture = Content.Load<Texture2D>("deer_still");
+            _position = new Vector2(20, 300);
+
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            //    Exit();
 
             // TODO: Add your update logic here
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                _position.Y += 3;
+                _position.Y -= 3;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                _position.X += 2;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                _position.X -= 2;
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.LightSkyBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_texture, _position, null, Color.White, 0f,
+            Vector2.Zero, 2f, SpriteEffects.None, 0f);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
